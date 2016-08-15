7bef6a4045267f867d32d3b9a21011e2590908e6
Rafael Laurindo <rafaelplaurindo@gmail.com>
2016-08-14 21:36:31 -0300

    fix decimal module

--- lib/measurement_units/is/decimal.rb
+++ lib/measurement_units/is/decimal.rb
@@ -11,34 +11,43 @@ module MeasurementUnits
 
       MULTIPLE = 10
 
-      def to_units_before value, units
+      def to_units_before value, units, precision = nil
         value = value.to_f
-        tester = MULTIPLE ** units
-        value = (value < tester ? (value * tester).round : value).to_f.formated
+        divisor = MULTIPLE ** units
+
+        # value = (value / divisor).round(precision).to_f.formated
+        if precision
+          value = (value / divisor).round(precision).to_f.formated
+        else
+          value = (value / divisor).to_f.formated
+        end
       end
 
-      def to_units_after value, units
+      def to_units_after value, units, precision = nil
         value = value.to_f
-        tester = MULTIPLE ** units
-        value = (value >= tester ? value / tester : value).to_f.formated
+        divisor = MULTIPLE ** units
+
+        # value = (value < tester ? (value * tester).round : value).to_f.formated
+        if precision
+          value = (value * divisor).round(precision).to_f.formated
+        else
+          value = (value * divisor).to_f.formated
+        end
       end
 
     end
   end
 end
 
-# # kg to gr, for example
-# p MeasurementUnits::IS::Decimal.to_units_before 0.001, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 0.01, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 0.1, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 1, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 1.001, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 1.010, 3
-# p MeasurementUnits::IS::Decimal.to_units_before 1.100, 3
+# kg     hg    dag  g dg cg  mg
+# 0.0001 0.001 0.01 1 10 100 1000
 
-# # gr to kg, for example
-# p MeasurementUnits::IS::Decimal.to_units_after 999, 3
-# p MeasurementUnits::IS::Decimal.to_units_after 1000, 3
-# p MeasurementUnits::IS::Decimal.to_units_after 1001, 3
-# p MeasurementUnits::IS::Decimal.to_units_after 1010, 3
-# p MeasurementUnits::IS::Decimal.to_units_after 1100, 3
+# Examples
+
+# kg to gr
+p MeasurementUnits::IS::Decimal.to_units_after 1, 3
+
+# mg to g
+p MeasurementUnits::IS::Decimal.to_units_before 1200, 3
+# returns 1.2
+# Lê-se um grama e dois decigramas, ou vinte centigramas ou duzentas (concorda com a uniadde) miligramas
